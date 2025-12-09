7ab7d1ab9a96426056830ce0df76862711244377
Gagan Pulse <gagandeep@pulseenergy.io>
2025-12-09 15:26:31 +0530

    Add new types for Init and OnInit actions in EV Charging BPP
    - Introduced `ExtractedInitRequestPayload` and `ExtractedOnInitResponsePayload` types to define the structure of initialization requests and responses.
    - Updated `InitPayload` and `OnInitPayload` to include new order types with detailed attributes for better data handling.
    - Removed the obsolete `AppOnSelectResponsePayload` type to streamline the schema.

--- src/ubc/schema/v2.0.0/actions/init/types/OnInitPayload.ts
+++ src/ubc/schema/v2.0.0/actions/init/types/OnInitPayload.ts
@@ -1,8 +1,12 @@
+import { ObjectType } from "../../../enums/ObjectType";
+import { BecknBuyer } from "../../../types/Buyer";
 import { Context } from "../../../types/Context";
-import { UBCOrder } from "../../../types/Order";
+import { BecknFulfillment } from "../../../types/Fulfillment";
+import { BecknOrderAttributes } from "../../../types/OrderAttributes";
+import { BecknOrderItemResponse } from "../../../types/OrderItem";
+import { BecknOrderValueResponse } from "../../../types/OrderValue";
+import { BecknPayment } from "../../../types/Payment";
 
-
-export type UBCOnInitOrder = Omit<UBCOrder, 'beckn:orderNumber' | 'beckn:payment'> & {};
 
 export type UBCOnInitRequestPayload = {
     context: Context;
@@ -10,3 +14,18 @@ export type UBCOnInitRequestPayload = {
         order: UBCOnInitOrder;
     };
 };
+
+export type UBCOnInitOrder = {
+    "@context": string;
+    "@type": ObjectType.order;
+    "beckn:id": string;
+    "beckn:orderStatus": string;
+    "beckn:orderNumber": string;
+    "beckn:seller": string;
+    "beckn:buyer": BecknBuyer;
+    "beckn:orderItems": BecknOrderItemResponse[];
+    "beckn:orderValue": BecknOrderValueResponse;
+    "beckn:payment": BecknPayment;
+    "beckn:fulfillment": BecknFulfillment;
+    "beckn:orderAttributes": BecknOrderAttributes;
+}
