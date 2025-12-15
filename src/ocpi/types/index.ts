13ca2fef6bf794dfb7b392aa20ec8e7dc43ac3fc
yash-pulse-energy <yash@pulseenergy.io>
2025-12-15 12:10:40 +0530

    OCPI log updated

--- src/ocpi/types/index.ts
+++ src/ocpi/types/index.ts
@@ -381,3 +381,78 @@ export interface OCPICommandResponse {
     timeout?: number;
     message?: string;
 }
+
+export enum OCPILogCommand {
+    // Generic
+    SendGetRes = 'SendGetRes',
+    SendGetReq = 'SendGetReq',
+
+    // Versions
+    GetVersionRes = 'GetVersionRes',
+    GetVersionReq = 'GetVersionReq',
+    GetVersionDetailsRes = 'GetVersionDetailsRes',
+    GetVersionDetailsReq = 'GetVersionDetailsReq',
+    GetSessionRes = 'GetSessionRes',
+    GetSessionReq = 'GetSessionReq',
+    SendGetVersionRes = 'SendGetVersionRes',
+    SendGetVersionReq = 'SendGetVersionReq',
+
+    // Credentials
+    PostCredentialsReq = 'PostCredentialsReq',
+    SendPostCredentialRes = 'SendPostCredentialRes',
+    SendPostCredentialReq = 'SendPostCredentialReq',
+    SendGetCredentialReq = 'SendGetCredentialReq',
+
+    // Tariffs
+    SendGetTariffRes = 'SendGetTariffRes',
+    SendGetTariffReq = 'SendGetTariffReq',
+    PutTariffRes = 'PutTariffRes',
+    PutTariffReq = 'PutTariffReq',
+    PostTariffRes = 'PostTariffRes',
+    PostTariffReq = 'PostTariffReq',
+
+    // Locations
+    PutLocationReq = 'PutLocationReq',
+    PutEVSEReq = 'PutEVSEReq',
+    PutConnectorReq = 'PutConnectorReq',
+    PostLocationRes = 'PostLocationRes',
+    PostLocationReq = 'PostLocationReq',
+    PatchLocationRes = 'PatchLocationRes',
+    PatchLocationReq = 'PatchLocationReq',
+    PatchEVSEReq = 'PatchEVSEReq',
+    PatchConnectorReq = 'PatchConnectorReq',
+    SendGetLocationRes = 'SendGetLocationRes',
+    SendGetLocationReq = 'SendGetLocationReq',
+
+    // Commands
+    PostStartSessionCommand = 'PostStartSessionCommand',
+    PostStopSessionCommand = 'PostStopSessionCommand',
+    StopSessionPostCommandResultRes = 'StopSessionPostCommandResultRes',
+    StopSessionPostCommandResultReq = 'StopSessionPostCommandResultReq',
+    StartSessionPostCommandResultRes = 'StartSessionPostCommandResultRes',
+    StartSessionPostCommandResultReq = 'StartSessionPostCommandResultReq',
+
+    // Sessions
+    SendStopSessionPostCommandRes = 'SendStopSessionPostCommandRes',
+    SendStopSessionPostCommandReq = 'SendStopSessionPostCommandReq',
+    SendStartSessionPostCommandRes = 'SendStartSessionPostCommandRes',
+    SendStartSessionPostCommandReq = 'SendStartSessionPostCommandReq',
+    SendGetSessionRes = 'SendGetSessionRes',
+    SendGetSessionReq = 'SendGetSessionReq',
+    PutSessionRes = 'PutSessionRes',
+    PutSessionReq = 'PutSessionReq',
+    PostSessionRes = 'PostSessionRes',
+    PostSessionReq = 'PostSessionReq',
+    PatchSessionRes = 'PatchSessionRes',
+    PatchSessionReq = 'PatchSessionReq',
+
+    // CDRs
+    PostCdrRes = 'PostCdrRes',
+    PostCdrReq = 'PostCdrReq',
+    GetCdrsReq = 'GetCdrReq',
+    GetCdrsRes = 'GetCdrRes',
+
+    // Tokens
+    PutTokenReq = 'PutTokenReq',
+    PatchTokenReq = 'PatchTokenReq',
+}
