c78f4faa8747102901aab81a62ddb836faa04ecd
Gagan Pulse <gagandeep@pulseenergy.io>
2025-12-16 13:46:34 +0530

    Refactor ChargingService to improve code readability and add handleActionOnChargingCompleted method for processing completed charging sessions. Enhance logging for better traceability of actions.

--- src/ubc/actions/services/ChargingService.ts
+++ src/ubc/actions/services/ChargingService.ts
@@ -1,15 +1,13 @@
-import PaymentTxnDbService from "../../../db-services/PaymentTxnDbService";
-import { ChargingSessionStatus } from "../../schema/v2.0.0/enums/ChargingSessionStatus";
-import { Session } from "@prisma/client";
-import AdminCommandsModule from "../../../admin/modules/AdminCommandsModule";
+import PaymentTxnDbService from '../../../db-services/PaymentTxnDbService';
+import { ChargingSessionStatus } from '../../schema/v2.0.0/enums/ChargingSessionStatus';
+import { Session } from '@prisma/client';
+import AdminCommandsModule from '../../../admin/modules/AdminCommandsModule';
 import { Request } from 'express';
-import OnUpdateActionHandler from "../handlers/OnUpdateActionHandler";
-import { logger } from "../../../services/logger.service";
-
+import OnUpdateActionHandler from '../handlers/OnUpdateActionHandler';
+import { logger } from '../../../services/logger.service';
 
 export default class ChargingService {
     public static async autoCutOffChargingSession(session: Session): Promise<void> {
-        
         try {
             if (session.status !== ChargingSessionStatus.ACTIVE) {
                 return;
@@ -28,7 +26,10 @@ export default class ChargingService {
                 requested_energy_units = paymentTxn?.requested_energy_units?.toNumber() ?? 0;
             }
 
-            if(requested_energy_units === 0 || requested_energy_units < (session?.kwh?.toNumber() ?? 0)) {
+            if (
+                requested_energy_units === 0 ||
+                requested_energy_units < (session?.kwh?.toNumber() ?? 0)
+            ) {
                 const req = {
                     body: {
                         partner_id: session?.partner_id,
@@ -36,30 +37,87 @@ export default class ChargingService {
                     },
                 } as Request;
 
-                logger.debug(`🟡 ${authorization_reference} Sending stop charging request in autoCutOffChargingSession`, {
-                    data: { req },
-                });
-
+                logger.debug(
+                    `🟡 ${authorization_reference} Sending stop charging request in autoCutOffChargingSession`,
+                    {
+                        data: { req },
+                    }
+                );
 
                 const response = await AdminCommandsModule.stopCharging(req);
-                logger.debug(`🟢 ${authorization_reference} Sent stop charging request in autoCutOffChargingSession`, {
-                    data: { req, response },
+                logger.debug(
+                    `🟢 ${authorization_reference} Sent stop charging request in autoCutOffChargingSession`,
+                    {
+                        data: { req, response },
+                    }
+                );
+                const paymentTxn = await PaymentTxnDbService.getFirstByFilter({
+                    where: {
+                        authorization_reference: session.authorization_reference ?? '',
+                    },
                 });
+
+                const becknTransactionId = paymentTxn?.beckn_transaction_id ?? '';
+
                 await OnUpdateActionHandler.handleEVChargingUBCBppOnUpdateAction({
-                    beckn_transaction_id: session?.authorization_reference ?? '',
-                    beckn_order_id: session?.authorization_reference ?? '',
+                    beckn_transaction_id: becknTransactionId,
+                    beckn_order_id: paymentTxn?.authorization_reference ?? '',
                     session_status: ChargingSessionStatus.COMPLETED,
                 });
-                logger.debug(`🟢 ${authorization_reference} Sent on_update request in autoCutOffChargingSession`, {
-                    data: { req },
-                });
+                logger.debug(
+                    `🟢 ${authorization_reference} Sent on_update request in autoCutOffChargingSession`,
+                    {
+                        data: {
+                            beckn_transaction_id: becknTransactionId,
+                            beckn_order_id: paymentTxn?.authorization_reference ?? '',
+                            session_status: ChargingSessionStatus.COMPLETED,
+                        },
+                    }
+                );
             }
-        }
-        
+        } 
         catch (error: any) {
             logger.error(`🔴 Error in autoCutOffChargingSession`, error, {
                 data: { message: 'Something went wrong' },
             });
+        }
+    }
+
+    public static async handleActionOnChargingCompleted(
+        authorization_reference: string
+    ): Promise<void> {
+        try {
+            const paymentTxn = await PaymentTxnDbService.getFirstByFilter({
+                where: {
+                    authorization_reference: authorization_reference,
+                },
+            });
+
+            const becknTransactionId = paymentTxn?.beckn_transaction_id ?? '';
+            await OnUpdateActionHandler.handleEVChargingUBCBppOnUpdateAction({
+                beckn_transaction_id: becknTransactionId,
+                beckn_order_id: paymentTxn?.authorization_reference ?? '',
+                session_status: ChargingSessionStatus.COMPLETED,
+            });
+            logger.debug(
+                `🟢 ${authorization_reference} Sent on_update request in autoCutOffChargingSession`,
+                {
+                    data: {
+                        beckn_transaction_id: becknTransactionId,
+                        beckn_order_id: paymentTxn?.authorization_reference ?? '',
+                        session_status: ChargingSessionStatus.COMPLETED,
+                    },
+                }
+            );
+        } 
+        catch (error: any) {
+            logger.error(
+                `🔴 ${authorization_reference} Error in handleActionOnChargingCompleted`,
+                error,
+                {
+                    data: { message: 'Something went wrong' },
+                }
+            );
         }
     }
 }
