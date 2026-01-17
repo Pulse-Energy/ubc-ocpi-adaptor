a8974ad90bf606e14c68fdad93e844f38a37b607
Gagan Pulse <gagandeep@pulseenergy.io>
2026-01-17 16:35:58 +0530

    Remove commented-out code for TrackActionHandler.sendOnTrackToBAPONIX in OCPIv221SessionsModuleIncomingRequestService to streamline session handling logic.

--- src/ocpi/modules/v2.2.1/emsp/sessions/OCPIv221SessionsModuleIncomingRequestService.ts
+++ src/ocpi/modules/v2.2.1/emsp/sessions/OCPIv221SessionsModuleIncomingRequestService.ts
@@ -354,7 +354,7 @@ export default class OCPIv221SessionsModuleIncomingRequestService {
                     logger.debug(`🟢 [${reqId}] Updated existing session in handlePutSession`, { 
                         data: { logData, sessionId: stored.id } 
                     });
-                    // TrackActionHandler.sendOnTrackToBAPONIX(stored?.authorization_reference ?? '');
+                    TrackActionHandler.sendOnTrackToBAPONIX(stored?.authorization_reference ?? '');
                 }
                 else {
                     logger.debug(`🟡 [${reqId}] No changes detected, using existing session in handlePutSession`, { data: logData });
@@ -549,7 +549,7 @@ export default class OCPIv221SessionsModuleIncomingRequestService {
                     data: { logData, sessionId: stored.id } 
                 });
 
-                // TrackActionHandler.sendOnTrackToBAPONIX(stored?.authorization_reference ?? '');
+                TrackActionHandler.sendOnTrackToBAPONIX(stored?.authorization_reference ?? '');
             }
 
             logger.debug(`🟡 [${reqId}] Mapping Prisma session to OCPI format in handlePatchSession`, { data: logData });
