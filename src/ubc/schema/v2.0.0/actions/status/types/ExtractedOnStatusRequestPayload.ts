36f02544f4bb5be112da3972cad3659bba52aeb8
Gagan Pulse <gagandeep@pulseenergy.io>
2026-01-19 16:44:05 +0530

    Update ExtractedOnStatusRequestBody to change payment_status type from BecknPaymentStatus to string for improved flexibility in status handling.

--- src/ubc/schema/v2.0.0/actions/status/types/ExtractedOnStatusRequestPayload.ts
+++ src/ubc/schema/v2.0.0/actions/status/types/ExtractedOnStatusRequestPayload.ts
@@ -2,7 +2,7 @@ import { GenericPaymentTxnStatus } from "../../../../../../types/BillDesk";
 import { BecknPaymentStatus } from "../../../enums/PaymentStatus";
 
 export type ExtractedOnStatusRequestBody = {
-    payment_status: BecknPaymentStatus,
+    payment_status: string,
     authorization_reference: string,
     oldPaymentStatus: GenericPaymentTxnStatus,
 };
